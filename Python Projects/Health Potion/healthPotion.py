41eecc5d80438765e4b79489b894a5178cd5ae5a
ambersweep <applemix5@gmail.com>
2022-05-19 19:51:46 -0400

    commit to main

--- Python Projects/Health Potion/healthPotion.py
+++ Python Projects/Health Potion/healthPotion.py
@@ -12,5 +12,5 @@ potionHealth = random.randint(25,50)
 #gives health its health potion and scales it with the difficulty level by dividing it by the difficulty. 
 #int gets rid of decimal by changing it from a float to an integer
 health = health + int(potionHealth/difficulty)
-
+#prints result
 print(health)
